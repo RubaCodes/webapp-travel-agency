c9fa29b73b142f6a5e8616c9bf856fc37428fa98
RubaCodes <rubacodes@gmail.com>
2022-10-20 17:39:33 +0200

    creato endpoint Getall per i pacchetti di viaggio

---
+++ Controllers/Api/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapp_travel_agency.Data.Repository;
+
+namespace webapp_travel_agency.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        DbTravelPackageRepo repo;
+        public HomeController(DbTravelPackageRepo repo)
+        {
+            this.repo = repo;
+        }
+        [HttpGet]
+        public IActionResult GetAll() {
+
+            var pkts = repo.GetAll();
+            if (pkts == null) {
+                return NoContent();
+            }
+            return Ok(pkts);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 using webapp_travel_agency.Data;
 using webapp_travel_agency.Data.Repository;
 
@@ -10,10 +11,13 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+// evitare loop nelle inclusioni delle tabelle secondarie
+builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+//dependency injection delle interfacce repo
 builder.Services.AddScoped<ApplicationDbContext>();
 builder.Services.AddScoped<DbDestinationRepo>();
 builder.Services.AddScoped<DbCategoryRepo>();
